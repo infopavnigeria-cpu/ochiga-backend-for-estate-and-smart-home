adb2791f5220302cae79dcfbf1453ce6692a6750
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 15:02:14 +0100

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  private users = [
+    { id: 1, name: 'Jane Doe' },
+    { id: 2, name: 'John Doe' }
+  ];
 
   @Get()
+  getAllUsers(): any {
+    return this.users;
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto): any {
+    const newUser = {
+      id: this.users.length + 1,
+      ...createUserDto
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
   getAllUsers() {
     return this.userService.getUsers();
   }
