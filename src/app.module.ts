7f6957d3a443bace7e9038d5a98f01e95d9638f8
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 11:48:50 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'ochiga_db',
-      autoLoadEntities: true,
-      synchronize: true, // Turn off in production
-    }),
+    // Commented out for now:
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'postgres',
+    //   database: 'ochiga_db',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
     UserModule,
   ],
   controllers: [AppController],
