539d5c4069c55e52f61b7896b8c75416d17b386f
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 11:39:49 +0100

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  getAllUsers(): any {
+    return [
+      { id: 1, name: 'Jane Doe' },
+      { id: 2, name: 'John Doe' }
+    ];
+  }
+}
