9c2fa767be7a86e21ec7417c9c7e1285f3d61d08
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 11:41:17 +0100

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  getAllUsers() {
+    return [
+      { id: 1, name: 'Ada' },
+      { id: 2, name: 'Emeka' },
+    ];
+  }
+}
