b95de99d3d1f03f4d3f87f1344bf7af3adfd7b34
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 12:41:56 +0100

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
+  private users = [
+    { id: 1, name: 'Jane Doe' },
+    { id: 2, name: 'John Doe' }
+  ];
+
   @Get()
   getAllUsers(): any {
-    return [
-      { id: 1, name: 'Jane Doe' },
-      { id: 2, name: 'John Doe' }
-    ];
+    return this.users;
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): any {
+    const user = this.users.find(u => u.id === parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 }
