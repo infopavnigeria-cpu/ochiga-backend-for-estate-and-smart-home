5820bb59e6ea75296aa110b9404c4e452bd171eb
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 21:06:20 +0100

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to Ochiga Smart Home Backend!';
+    return 'Ochiga backend is active 🚀';
   }
 }
