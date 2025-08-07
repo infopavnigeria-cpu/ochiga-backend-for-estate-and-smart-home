dbd22cf6f1cadfb495468f7cae35ddc2573abff9
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 21:15:39 +0100

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return 'Ochiga backend is active 🚀';
+  getStatus(): string {
+    return this.appService.getStatus();
   }
 }
