1445d8c9f319938bbc32888d5732454da466c074
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-07 09:41:22 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HelloController } from './hello.controller'; // 👈 Add this line
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HelloController], // 👈 Register both controllers
   providers: [AppService],
 })
 export class AppModule {}
