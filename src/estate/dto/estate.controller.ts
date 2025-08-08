898ad569d6719abd9aa5656caec1e01c04463a3c
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-08 20:22:01 +0000

    Update estate.controller.ts

--- src/estate/dto/estate.controller.ts
+++ src/estate/dto/estate.controller.ts
@@ -1,19 +1,34 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { EstateService } from './estate.service';
 import { CreateEstateDto } from './dto/create-estate.dto';
-import { Estate } from './estate.entity';
+import { UpdateEstateDto } from './dto/update-estate.dto';
 
 @Controller('estate')
 export class EstateController {
   constructor(private readonly estateService: EstateService) {}
 
   @Post()
-  create(@Body() dto: CreateEstateDto): Estate {
-    return this.estateService.create(dto);
+  create(@Body() createEstateDto: CreateEstateDto) {
+    return this.estateService.create(createEstateDto);
   }
 
   @Get()
-  findAll(): Estate[] {
+  findAll() {
     return this.estateService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.estateService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateEstateDto: UpdateEstateDto) {
+    return this.estateService.update(+id, updateEstateDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.estateService.remove(+id);
   }
 }
