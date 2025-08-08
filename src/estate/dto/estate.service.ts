0d0478a210b8915160899b41f5397605152f9f03
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-08 21:03:49 +0100

    Update estate.service.ts

--- src/estate/dto/estate.service.ts
+++ src/estate/dto/estate.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEstateDto } from './dto/create-estate.dto';
-import { Estate } from './estate.entity';
+import { UpdateEstateDto } from './dto/update-estate.dto';
 
 @Injectable()
 export class EstateService {
-  private estates: Estate[] = [];
+  private estates = [];
 
-  create(dto: CreateEstateDto): Estate {
-    const estate: Estate = {
-      id: this.estates.length + 1,
-      ...dto,
-    };
-    this.estates.push(estate);
-    return estate;
+  create(createEstateDto: CreateEstateDto) {
+    const newEstate = { id: Date.now(), ...createEstateDto };
+    this.estates.push(newEstate);
+    return newEstate;
   }
 
-  findAll(): Estate[] {
+  findAll() {
     return this.estates;
+  }
+
+  findOne(id: number) {
+    return this.estates.find(estate => estate.id === id);
+  }
+
+  update(id: number, updateEstateDto: UpdateEstateDto) {
+    const index = this.estates.findIndex(estate => estate.id === id);
+    if (index > -1) {
+      this.estates[index] = { ...this.estates[index], ...updateEstateDto };
+      return this.estates[index];
+    }
+    return null;
+  }
+
+  remove(id: number) {
+    const index = this.estates.findIndex(estate => estate.id === id);
+    if (index > -1) {
+      const deleted = this.estates.splice(index, 1);
+      return deleted[0];
+    }
+    return null;
   }
 }
