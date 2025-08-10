997f6e3ae9a63e200fe43dec920e14c5708efca1
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-10 12:29:44 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
-// src/estate/estate.module.ts
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { UserModule } from './user/user.module';
 import { EstateModule } from './estate/estate.module';
 import { Estate } from './estate/entities/estate.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite', // You can switch to 'postgres' or 'mysql'
+      type: 'sqlite',
       database: 'db.sqlite',
-      entities: [Estate],
-      synchronize: true, // Auto-create tables (good for dev only)
+      entities: [Estate], // You can add other entities here like User
+      synchronize: true, // Only for development
     }),
-    EstateModule,
+    UserModule,      // Correctly import and add the UserModule
+    EstateModule,    // Correctly import and add the EstateModule
   ],
   controllers: [AppController],
   providers: [AppService],
