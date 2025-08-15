47d54790469e4ce0e5803a33169faa85377e3f8d
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 04:25:17 +0100

    Update estate.entity.ts

--- src/estate/entities/estate.entity.ts
+++ src/estate/entities/estate.entity.ts
@@ -4,20 +4,17 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class Estate {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number; // Auto-generated ID
 
   @Column()
-  name: string;
+  name!: string; // Estate name
 
   @Column()
-  location: string;
+  location!: string; // Estate location
 
   @Column({ nullable: true })
-  size?: number;
-
-  @Column({ nullable: true })
-  description?: string;
+  description?: string; // Optional description
 
   @Column({ default: true })
-  active: boolean;
+  active!: boolean; // Estate status (default: active)
 }
