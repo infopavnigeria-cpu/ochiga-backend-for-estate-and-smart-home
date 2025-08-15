74b732882c61ce4652b3afd32464093aa8d69eca
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 19:50:06 +0100

    Update room.service.ts

--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,44 +1,17 @@
-import { Injectable, NotFoundException, ForbiddenException } from '@nestjs/common';
-import { CreateRoomDto } from './dto/create-room.dto';
-import { UpdateRoomDto } from './dto/update-room.dto';
-import { Room } from './entities/room.entity';
-import { HomeService } from '../home/home.service';
+async create(userId: number, createRoomDto: CreateRoomDto) {
+  // Validate ownership before creating
+  await this.homeService.findOne(userId, createRoomDto.homeId);
 
-@Injectable()
-export class RoomService {
-  private rooms: Room[] = [];
-  private idCounter = 1;
+  const room = this.roomRepository.create(createRoomDto);
+  return this.roomRepository.save(room);
+}
 
-  constructor(private readonly homeService: HomeService) {}
+async update(userId: number, id: number, updateRoomDto: UpdateRoomDto) {
+  const index = await this.findIndexById(id);
 
-  create(userId: number, dto: CreateRoomDto): Room {
-    const home = this.homeService.findOne(userId, dto.homeId); // validates ownership
-    const room: Room = {
-      id: this.idCounter++,
-      name: dto.name,
-      homeId: home.id
-    };
-    this.rooms.push(room);
-    return room;
-  }
+  // Validate ownership before updating
+  await this.homeService.findOne(userId, this.rooms[index].homeId);
 
-  findByHome(userId: number, homeId: number): Room[] {
-    this.homeService.findOne(userId, homeId); // validates ownership
-    return this.rooms.filter(room => room.homeId === homeId);
-  }
-
-  update(userId: number, id: number, dto: UpdateRoomDto): Room {
-    const room = this.rooms.find(r => r.id === id);
-    if (!room) throw new NotFoundException('Room not found');
-    this.homeService.findOne(userId, room.homeId); // validates ownership
-    Object.assign(room, dto);
-    return room;
-  }
-
-  remove(userId: number, id: number): void {
-    const index = this.rooms.findIndex(r => r.id === id);
-    if (index === -1) throw new NotFoundException('Room not found');
-    this.homeService.findOne(userId, this.rooms[index].homeId); // validates ownership
-    this.rooms.splice(index, 1);
-  }
+  Object.assign(this.rooms[index], updateRoomDto);
+  return this.roomRepository.save(this.rooms[index]);
 }
