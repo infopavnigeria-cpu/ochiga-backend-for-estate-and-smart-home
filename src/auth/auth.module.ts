2c7e4eb60a670d39b3fa77427b4d1ab8721e2866
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 14:27:22 +0100

    Create auth.module.ts

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'YOUR_SECRET_KEY', // IMPORTANT: Use a secure, environment variable for this key
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
