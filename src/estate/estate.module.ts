4e34d5c0497e914ce1c9f7eb97c4968d2fdd47bd
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 05:45:53 +0100

    Update estate.module.ts

--- src/estate/estate.module.ts
+++ src/estate/estate.module.ts
@@ -1,8 +1,12 @@
+// src/estate/estate.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstateService } from './estate.service';
 import { EstateController } from './estate.controller';
+import { Estate } from './entities/estate.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Estate])],
   controllers: [EstateController],
   providers: [EstateService],
 })
