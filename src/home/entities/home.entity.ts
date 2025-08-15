09b2c51211bed4e8af4bf7df0ad9b35911b57a2f
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 19:27:28 +0100

    Update home.entity.ts

--- src/home/entities/home.entity.ts
+++ src/home/entities/home.entity.ts
@@ -1,6 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Room } from '../room/room.entity';
+
+@Entity()
 export class Home {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
-  address: string;
-  userId: number; // Owner user ID
+
+  @ManyToOne(() => User, (user) => user.homes)
+  owner: User;
+
+  @OneToMany(() => Room, (room) => room.home, { cascade: true })
+  rooms: Room[];
 }
