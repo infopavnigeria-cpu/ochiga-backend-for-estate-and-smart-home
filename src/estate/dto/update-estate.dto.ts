5904b7871aa075807f1e7dafa91bc03667bb0b75
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 05:39:13 +0100

    Update update-estate.dto.ts

--- src/estate/dto/update-estate.dto.ts
+++ src/estate/dto/update-estate.dto.ts
@@ -1,5 +1,37 @@
-// src/estate/dto/update-estate.dto.ts
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEstateDto } from './create-estate.dto';
+import { IsString, IsOptional, IsBoolean, IsNumber } from 'class-validator';
 
-export class UpdateEstateDto extends PartialType(CreateEstateDto) {}
+export class UpdateEstateDto extends PartialType(CreateEstateDto) {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  location?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  active?: boolean;
+
+  @IsOptional()
+  @IsString()
+  managerName?: string;
+
+  @IsOptional()
+  @IsString()
+  contactNumber?: string;
+
+  @IsOptional()
+  @IsNumber()
+  totalUnits?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  smartIntegration?: boolean;
+}
