4b82bfe1877aca450cd5fa629c8ad0e2c5604b9f
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 05:38:31 +0100

    Update create-estate.dto.ts

--- src/estate/dto/create-estate.dto.ts
+++ src/estate/dto/create-estate.dto.ts
@@ -1,11 +1,33 @@
-// src/estate/dto/create-estate.dto.ts
+import { IsString, IsOptional, IsBoolean, IsNumber } from 'class-validator';
+
 export class CreateEstateDto {
+  @IsString()
   name!: string;
+
+  @IsString()
   location!: string;
+
+  @IsOptional()
+  @IsString()
   description?: string;
+
+  @IsOptional()
+  @IsBoolean()
   active?: boolean;
+
+  @IsOptional()
+  @IsString()
   managerName?: string;
+
+  @IsOptional()
+  @IsString()
   contactNumber?: string;
+
+  @IsOptional()
+  @IsNumber()
   totalUnits?: number;
+
+  @IsOptional()
+  @IsBoolean()
   smartIntegration?: boolean;
 }
