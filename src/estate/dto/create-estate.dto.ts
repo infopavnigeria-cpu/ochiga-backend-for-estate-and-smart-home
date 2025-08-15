1fd7930df8b778b3f4019285580cb8e85c38434f
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-15 05:24:42 +0100

    Update create-estate.dto.ts

--- src/estate/dto/create-estate.dto.ts
+++ src/estate/dto/create-estate.dto.ts
@@ -1,6 +1,11 @@
 // src/estate/dto/create-estate.dto.ts
 export class CreateEstateDto {
-  name!: string;
-  location!: string;
-  size?: number; // optional
+  name: string;
+  location: string;
+  description?: string;
+  active?: boolean;
+  managerName?: string;
+  contactNumber?: string;
+  totalUnits?: number;
+  smartIntegration?: boolean;
 }
