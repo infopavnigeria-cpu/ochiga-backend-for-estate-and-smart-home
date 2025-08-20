674165035a725d9cc8f5e9ba9f12e06205d39596
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-20 15:25:06 +0100

    Update update-home.dto.ts

--- src/home/dto/update-home.dto.ts
+++ src/home/dto/update-home.dto.ts
@@ -1,3 +1,10 @@
+// src/home/dto/create-home.dto.ts
+export class CreateHomeDto {
+  name!: string;
+  address!: string;
+}
+
+// src/home/dto/update-home.dto.ts
 export class UpdateHomeDto {
   name?: string;
   address?: string;
