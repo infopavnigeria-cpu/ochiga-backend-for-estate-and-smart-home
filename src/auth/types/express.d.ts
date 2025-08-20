c78e1e4785e6fbfa78e7478b23327468c381a604
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-20 15:37:29 +0100

    Create express.d.ts

---
+++ src/auth/types/express.d.ts
@@ -0,0 +1,8 @@
+// src/auth/types/express.d.ts
+import { User } from '../../user/entities/user.entity';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: User;
+  }
+}
