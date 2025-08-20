8b8b6aa877be02be95eb7e3847ebb7820a9e2c32
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-20 13:04:02 +0100

    Update home.entity.ts

--- src/home/entities/home.entity.ts
+++ src/home/entities/home.entity.ts
@@ -1,16 +1,8 @@
-// src/home/entities/home.entity.ts
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  ManyToOne,
-  OneToMany,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
 import { Estate } from '../../estate/entities/estate.entity';
 import { Room } from '../room/room.entity';
 import { User } from '../../user/entities/user.entity';
-
-entity';
+import { HomeMember } from './home-member.entity';
 
 @Entity()
 export class Home {
@@ -23,9 +15,11 @@ export class Home {
   @ManyToOne(() => Estate, (estate) => estate.homes, { onDelete: 'CASCADE' })
   estate!: Estate;
 
-  @ManyToOne(() => User, (user) => user.homes, { onDelete: 'CASCADE' })
-  user!: User;   // 👈 new relation
+  // One home has many members (owner, admins, moderators, etc.)
+  @OneToMany(() => HomeMember, (member) => member.home, { cascade: true })
+  members!: HomeMember[];
 
   @OneToMany(() => Room, (room) => room.home, { cascade: true })
   rooms!: Room[];
+}
 }
