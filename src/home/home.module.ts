50a4249fe5a1f51e450a65b3b4df65a1db6440e1
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-20 14:15:58 +0100

    Update home.module.ts

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Home } from './home.entity';
+import { Home } from './entities/home.entity';
+import { HomeMember } from './entities/home-member.entity'; // if you created HomeMember
 import { HomeService } from './home.service';
 import { HomeController } from './home.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Home])],
+  imports: [TypeOrmModule.forFeature([Home, HomeMember])], // register both if you use HomeMember
   providers: [HomeService],
   controllers: [HomeController],
   exports: [HomeService],
