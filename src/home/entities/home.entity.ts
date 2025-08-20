df4ce813525600313f0fc8602136dd30b41dd451
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-20 09:01:22 +0100

    Update home.entity.ts

--- src/home/entities/home.entity.ts
+++ src/home/entities/home.entity.ts
@@ -1,8 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+// src/home/entities/home.entity.ts
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
 import { Estate } from '../../estate/entities/estate.entity';
 import { Room } from '../room/room.entity';
+import { User } from '../../user/entities/user.entity'; // 👈 add this
 
-@Entity()
+@Entity('homes') // 👈 name your table
 export class Home {
   @PrimaryGeneratedColumn()
   id!: number;
@@ -10,9 +18,15 @@ export class Home {
   @Column()
   name!: string;
 
+  // Relation → Estate
   @ManyToOne(() => Estate, (estate) => estate.homes, { onDelete: 'CASCADE' })
   estate!: Estate;
 
+  // Relation → Rooms
   @OneToMany(() => Room, (room) => room.home, { cascade: true })
   rooms!: Room[];
+
+  // Relation → User (owner)
+  @ManyToOne(() => User, (user) => user.homes, { onDelete: 'SET NULL' })
+  owner?: User;
 }
