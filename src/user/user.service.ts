914d87eec21c5f0d76deb9a896dcd018b257f87e
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-08-31 23:45:06 +0100

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
+  private users = [
+    { id: 1, name: 'Ada' },
+    { id: 2, name: 'Emeka' },
+  ];
+
   getAllUsers() {
-    return [
-      { id: 1, name: 'Ada' },
-      { id: 2, name: 'Emeka' },
-    ];
+    return this.users;
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = {
+      id: this.users.length + 1,
+      ...createUserDto,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 }
