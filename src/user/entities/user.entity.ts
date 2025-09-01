63749c7902eb1c9112bc05305d327d8c9180056e
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-01 10:51:27 +0100

    Update user.entity.ts

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,11 @@
-// src/user/entities/user.entity.ts
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { HomeMember } from '../../home/entities/home-member.entity';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  RESIDENT = 'resident',
+  MANAGER = 'manager',
+}
 
 @Entity()
 export class User {
@@ -15,6 +20,13 @@ export class User {
 
   @Column()
   password!: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.RESIDENT,
+  })
+  role!: UserRole;
 
   @OneToMany(() => HomeMember, (member) => member.user)
   homeMembers!: HomeMember[];
