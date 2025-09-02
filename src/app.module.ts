2b688b072a9f2f5bd6bcd748ea701c46b7ce368e
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-02 10:53:03 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,19 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { EstateModule } from './estate/estate.module';
-import { HomeModule } from './home/home.module';
-import { RoomModule } from './room/room.module';
-import { AuthModule } from './auth/auth.module';
-
-import { User } from './user/entities/user.entity';
-import { Estate } from './estate/entities/estate.entity';
-import { Home } from './home/entities/home.entity';
-import { Room } from './room/entities/room.entity';
+import { User } from './users/user.entity';
+import { Estate } from './estates/estate.entity';
+import { Home } from './homes/home.entity';
+import { Room } from './rooms/room.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'ochiga_db',
+      type: 'sqlite',
+      database: 'db.sqlite',
       entities: [User, Estate, Home, Room],
-      synchronize: true, // ⚠️ keep true for dev, set to false in prod
-      logging: true,
+      synchronize: true,  // auto-create tables in dev
     }),
-    UserModule,
-    EstateModule,
-    HomeModule,
-    RoomModule,
-    AuthModule,
+    TypeOrmModule.forFeature([User, Estate, Home, Room]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
