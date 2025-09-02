3d75399d854c5df2c52741c0bc0310e1ca0f819b
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-02 11:05:31 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
 import { Estate } from './estate/entities/estate.entity';
 import { Home } from './home/entities/home.entity';
 import { Room } from './room/entities/room.entity';
+import { User } from './user/entities/user.entity';
+import { HomeMember } from './home/entities/home-member.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
+      type: 'sqlite', // or postgres
       database: 'db.sqlite',
-      entities: [User, Estate, Home, Room],
+      entities: [Estate, Home, Room, User, HomeMember],
       synchronize: true,
     }),
+    TypeOrmModule.forFeature([Estate, Home, Room, User, HomeMember]),
   ],
 })
-export class AppModule {}
