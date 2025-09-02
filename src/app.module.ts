f842280ea1233c7a9d4b3787057661057288a27b
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-02 11:07:51 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
-import { Estate } from './estate/entities/estate.entity';
-import { Home } from './home/entities/home.entity';
-import { Room } from './room/entities/room.entity';
-import { User } from './user/entities/user.entity';
-import { HomeMember } from './home/entities/home-member.entity';
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite', // or postgres
+      type: 'sqlite',
       database: 'db.sqlite',
       entities: [Estate, Home, Room, User, HomeMember],
       synchronize: true,
@@ -15,3 +9,5 @@ import { HomeMember } from './home/entities/home-member.entity';
     TypeOrmModule.forFeature([Estate, Home, Room, User, HomeMember]),
   ],
 })
+})
+export class AppModule {}
