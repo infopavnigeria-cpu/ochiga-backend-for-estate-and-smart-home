9bf15e9bf3aee56b9fd1ba5e7361f774a47d7d1e
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-03 23:08:25 +0100

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,17 @@
+// src/data-source.ts
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./user/entities/user.entity";
 import { Estate } from "./estate/entities/estate.entity";
 import { Home } from "./home/entities/home.entity";
 import { Room } from "./room/entities/room.entity";
+import { HomeMember } from "./home/entities/home-member.entity";
 
 export const AppDataSource = new DataSource({
-  type: "postgres", // or "mysql"
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "password",
-  database: "ochiga_db",
-  entities: [User, Estate, Home, Room],
-  migrations: ["dist/migrations/*.js"],
-  synchronize: false, // keep false in production
+  type: "sqlite",
+  database: "db.sqlite",
+  entities: [User, Estate, Home, Room, HomeMember],
+  migrations: ["src/migrations/*.ts"],
+  synchronize: false, // keep false for migrations
   logging: true,
 });
