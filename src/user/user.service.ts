60930e20b32b36547d66cb0f8d5e1d22a296bb42
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-12 22:32:26 +0100

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,8 +4,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UserService {
   private users = [
-    { id: 1, name: 'Ada' },
-    { id: 2, name: 'Emeka' },
+    { id: 1, estate: 'P2E Estate', name: 'Ada', house: 'B12', records: [], history: [] },
+    { id: 2, estate: 'GreenVille', name: 'Emeka', house: 'C4', records: [], history: [] },
   ];
 
   getAllUsers() {
@@ -15,9 +15,22 @@ export class UserService {
   createUser(createUserDto: CreateUserDto) {
     const newUser = {
       id: this.users.length + 1,
+      records: [],
+      history: [],
       ...createUserDto,
     };
     this.users.push(newUser);
     return newUser;
+  }
+
+  getUserById(id: number) {
+    return this.users.find(user => user.id === id);
+  }
+
+  updateUser(id: number, updateData: Partial<any>) {
+    const user = this.getUserById(id);
+    if (!user) return null;
+    Object.assign(user, updateData);
+    return user;
   }
 }
