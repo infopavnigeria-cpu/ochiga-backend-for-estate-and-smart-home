442725413faecc40ea64abd39b8baa45a15fa93f
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-12 23:45:42 +0100

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,5 @@
-// src/user/user.controller.ts
 import { Body, Controller, Get, Post, Param, Put } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { CreateResidentDto } from './dto/create-resident.dto';
-import { RegisterResidentDto } from './dto/register-resident.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -19,16 +16,6 @@ export class UserController {
     return this.userService.createUser(createUserDto);
   }
 
-  @Post('resident')
-  createResident(@Body() createResidentDto: CreateResidentDto) {
-    return this.userService.createResident(createResidentDto);
-  }
-
-  @Post('register')
-  registerResident(@Body() registerDto: RegisterResidentDto) {
-    return this.userService.registerResident(registerDto);
-  }
-
   @Get(':id')
   getUserById(@Param('id') id: string) {
     return this.userService.getUserById(Number(id));
@@ -37,5 +24,11 @@ export class UserController {
   @Put(':id')
   updateUser(@Param('id') id: string, @Body() updateData: any) {
     return this.userService.updateUser(Number(id), updateData);
+  }
+
+  // 🔑 New: Register endpoint
+  @Post('register')
+  registerResident(@Body() body: { inviteToken: string; password: string }) {
+    return this.userService.registerResident(body.inviteToken, body.password);
   }
 }
