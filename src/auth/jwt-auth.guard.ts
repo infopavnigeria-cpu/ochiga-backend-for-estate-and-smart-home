d546dc7f1cf3d9e2e815a9101169b6d504c7a275
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-13 23:14:33 +0100

    Create jwt-auth.guard.ts

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) throw new UnauthorizedException("No token provided");
+
+    const token = authHeader.split(" ")[1];
+    try {
+      const decoded = this.jwtService.verify(token);
+      request.user = decoded;
+      return true;
+    } catch (err) {
+      throw new UnauthorizedException("Invalid token");
+    }
+  }
+}
