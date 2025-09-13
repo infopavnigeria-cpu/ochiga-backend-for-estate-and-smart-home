48473f2b36383472a8c3893cf54b78646ded3b2a
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-13 23:49:23 +0100

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
+// src/user/user.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], // 👈 Gives UserModule access to User table
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService], // 👈 So AuthModule (and others) can reuse UserService
 })
 export class UserModule {}
