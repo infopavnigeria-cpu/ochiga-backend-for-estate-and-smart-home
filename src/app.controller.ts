d51d1b7df96965463fef06d2466cb39202587dad
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-14 17:00:49 +0100

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,10 @@
-// src/user/user.controller.ts
-import { Body, Controller, Get, Post, Param, Put } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserService } from './user.service';
+// src/app.controller.ts
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
+@Controller()
+export class AppController {
   @Get()
-  getAllUsers() {
-    return this.userService.getAllUsers();
-  }
-
-  @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
-  }
-
-  @Get(':id')
-  getUserById(@Param('id') id: string) {
-    return this.userService.getUserById(Number(id));
-  }
-
-  @Put(':id')
-  updateUser(@Param('id') id: string, @Body() updateData: any) {
-    return this.userService.updateUser(Number(id), updateData);
+  getHello(): string {
+    return 'Ochiga backend running 🚀';
   }
 }
