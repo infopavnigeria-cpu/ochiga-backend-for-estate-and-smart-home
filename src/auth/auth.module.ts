87839ebeb591f782e4d5b52f49d0b432e381550b
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-14 22:50:46 +0100

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-i// src/auth/auth.module.ts
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
