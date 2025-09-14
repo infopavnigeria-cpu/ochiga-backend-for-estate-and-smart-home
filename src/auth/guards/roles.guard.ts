3b6478aa1b547dfba010568e43fdee2bd1db32da
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-14 16:09:52 +0100

    Update roles.guard.ts

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../roles.decorator';
-import { UserRole } from '../../user/entities/user.entity';
+import { UserRole } from '../../enums/user-role.enum'; // ✅ fixed import
 
 @Injectable()
 export class RolesGuard implements CanActivate {
