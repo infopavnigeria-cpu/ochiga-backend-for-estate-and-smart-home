5f517af74d2817ca6f037eb83927765b83af4c33
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-14 02:17:56 +0100

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,23 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 
+export enum UserRole {
+  RESIDENT = 'resident',
+  MANAGER = 'manager',
+  ADMIN = 'admin',
+}
+
 export interface User {
   id: number;
   email: string;
   password: string;
+  role?: UserRole; // optional for now
 }
 
 @Injectable()
 export class UserService {
   private users: User[] = [];
 
-  createUser(data: { email: string; password: string }): User {
+  createUser(data: { email: string; password: string; role?: UserRole }): User {
     const exists = this.users.find(u => u.email === data.email);
     if (exists) {
       throw new BadRequestException('User already exists');
@@ -19,7 +26,8 @@ export class UserService {
     const newUser: User = {
       id: this.users.length + 1,
       email: data.email,
-      password: data.password, // 🔐 will hash later
+      password: data.password, // hash later
+      role: data.role || UserRole.RESIDENT,
     };
 
     this.users.push(newUser);
@@ -28,5 +36,9 @@ export class UserService {
 
   findByEmail(email: string): User | undefined {
     return this.users.find(u => u.email === email);
+  }
+
+  getUserById(id: number): User | undefined {
+    return this.users.find(u => u.id === id);
   }
 }
