a222a0625c9ddcff2e8f2c6867449d9d5c77210e
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-14 01:10:11 +0100

    Create create-manager.dto.ts

---
+++ src/user/dto/create-manager.dto.ts
@@ -0,0 +1,13 @@
+// src/user/dto/create-manager.dto.ts
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateManagerDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
