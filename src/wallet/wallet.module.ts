6e078dde60840c87a5541143f44dcf6e01aaf324
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 14:48:24 +0100

    Update wallet.module.ts

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,12 @@
-// src/wallet/wallet.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wallet } from './entities/wallet.entity';
+import { User } from '../user/entities/user.entity';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Wallet, User])],
   providers: [WalletService],
   controllers: [WalletController],
   exports: [WalletService],
