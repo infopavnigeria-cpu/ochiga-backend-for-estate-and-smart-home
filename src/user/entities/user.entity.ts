5875d0bd5ea2bfbd14611dd8d37eee4dba9190d1
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 16:50:41 +0100

    Update user.entity.ts

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -3,23 +3,20 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
-  OneToOne,
   OneToMany,
 } from 'typeorm';
-import { Wallet } from '../../wallet/entities/wallet.entity';
 import { Payment } from '../../payments/entities/payment.entity';
+import { Wallet } from '../../wallet/entities/wallet.entity';
+import { HomeMember } from '../../home/entities/home-member.entity';
 import { Visitor } from '../../visitors/visitors.entity';
 
-export enum UserRole {
-  ADMIN = 'ADMIN',
-  RESIDENT = 'RESIDENT',
-  MANAGER = 'MANAGER',
-}
-
-@Entity('users')
+@Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
+
+  @Column()
+  name!: string;
 
   @Column({ unique: true })
   email!: string;
@@ -27,18 +24,16 @@ export class User {
   @Column()
   password!: string;
 
-  @Column()
-  name!: string;
-
-  @Column({ type: 'enum', enum: UserRole, default: UserRole.RESIDENT })
-  role!: UserRole;
-
-  @OneToOne(() => Wallet, (wallet) => wallet.user)
-  wallet!: Wallet;
-
+  /** Relations */
   @OneToMany(() => Payment, (payment) => payment.user)
   payments!: Payment[];
 
-  @OneToMany(() => Visitor, (visitor) => visitor.invitedBy)
+  @OneToMany(() => Wallet, (wallet) => wallet.user)
+  wallets!: Wallet[];
+
+  @OneToMany(() => HomeMember, (homeMember) => homeMember.user)
+  homeMembers!: HomeMember[];
+
+  @OneToMany(() => Visitor, (visitor) => visitor.user)
   invitedVisitors!: Visitor[];
 }
