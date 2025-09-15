fe571ab85d7e62f61325fb4c263872196fabfe26
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 17:29:52 +0100

    Update wallet.entity.ts

--- src/wallet/entities/wallet.entity.ts
+++ src/wallet/entities/wallet.entity.ts
@@ -1,14 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { User } from '../../user/entities/user.entity';
+import { Payment } from '../../payments/entities/payment.entity';
 
 @Entity()
 export class Wallet {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @Column()
+  @Column('decimal', { precision: 12, scale: 2, default: 0 })
   balance!: number;
 
-  @ManyToOne(() => User, (user) => user.wallets, { onDelete: 'CASCADE' })
+  @Column({ default: 'NGN' })
+  currency!: string;
+
+  @OneToOne(() => User, (user) => user.wallet, { onDelete: 'CASCADE' })
+  @JoinColumn()
   user!: User;
+
+  @OneToOne(() => Payment, (payment) => payment.wallet)
+  payments!: Payment[];
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
 }
