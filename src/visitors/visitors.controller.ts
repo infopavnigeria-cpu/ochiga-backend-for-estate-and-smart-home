23d2786a373834ae0e70c693cbaf9e2b4a85cbf3
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 14:28:29 +0100

    Update visitors.controller.ts

--- src/visitors/visitors.controller.ts
+++ src/visitors/visitors.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Post, Get, Param, Body, Req, Delete } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body } from '@nestjs/common';
 import { VisitorsService } from './visitors.service';
 import { Visitor } from './visitors.entity';
-import { Request } from 'express';
 
 @Controller('visitors')
 export class VisitorsController {
   constructor(private readonly visitorsService: VisitorsService) {}
 
   @Post()
-  async create(@Req() req: Request, @Body() dto: Partial<Visitor>) {
-    const user: any = (req as any).user; // assume injected by AuthGuard
-    return this.visitorsService.create(user, dto);
+  async create(@Body() dto: Partial<Visitor>) {
+    return this.visitorsService.create(dto);
   }
 
   @Get()
@@ -21,10 +19,5 @@ export class VisitorsController {
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.visitorsService.findOne(id);
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return this.visitorsService.remove(id);
   }
 }
