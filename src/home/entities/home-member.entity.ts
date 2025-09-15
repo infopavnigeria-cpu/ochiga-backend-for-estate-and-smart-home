c3cbdd10cdde7cc4312d22cb3738124934efe57c
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 16:51:27 +0100

    Update home-member.entity.ts

--- src/home/entities/home-member.entity.ts
+++ src/home/entities/home-member.entity.ts
@@ -1,33 +1,23 @@
 // src/home/entities/home-member.entity.ts
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, CreateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+} from 'typeorm';
 import { User } from '../../user/entities/user.entity';
-import { Home } from './home.entity';
-
-export enum HomeRole {
-  OWNER = 'OWNER',
-  ADMIN = 'ADMIN',
-  MEMBER = 'MEMBER',
-}
 
 @Entity()
 export class HomeMember {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @ManyToOne(() => User, (user) => user.homeMembers, {
-    onDelete: 'CASCADE',
-    eager: true, // ✅ auto-load user info
-  })
+  @Column()
+  name!: string;
+
+  @Column()
+  role!: string; // e.g. Resident, Guest
+
+  @ManyToOne(() => User, (user) => user.homeMembers, { onDelete: 'CASCADE' })
   user!: User;
-
-  @ManyToOne(() => Home, (home) => home.members, {
-    onDelete: 'CASCADE',
-  })
-  home!: Home;
-
-  @Column({ type: 'text', default: HomeRole.MEMBER })
-  role!: HomeRole;
-
-  @CreateDateColumn()
-  joinedAt!: Date;
 }
