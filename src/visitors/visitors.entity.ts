4704805ceefac9ffb719746659b50866b842997d
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 17:08:35 +0100

    Update visitors.entity.ts

--- src/visitors/visitors.entity.ts
+++ src/visitors/visitors.entity.ts
@@ -1,11 +1,11 @@
-// src/visitors/visitors.entity.ts
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   ManyToOne,
+  CreateDateColumn,
 } from 'typeorm';
-import { User } from '../../user/entities/user.entity';
+import { User } from '../user/entities/user.entity';
 
 @Entity()
 export class Visitor {
@@ -13,11 +13,21 @@ export class Visitor {
   id!: string;
 
   @Column()
-  fullName!: string;
+  name!: string;
 
   @Column()
-  visitDate!: Date;
+  purpose!: string;
 
-  @ManyToOne(() => User, (user) => user.invitedVisitors, { eager: true, onDelete: 'CASCADE' })
+  @Column({ default: false })
+  approved!: boolean;
+
+  // 👥 Many visitors belong to one user (who invited them)
+  @ManyToOne(() => User, (user) => user.invitedVisitors, {
+    eager: true,
+    onDelete: 'CASCADE',
+  })
   user!: User;
+
+  @CreateDateColumn()
+  invitedAt!: Date;
 }
