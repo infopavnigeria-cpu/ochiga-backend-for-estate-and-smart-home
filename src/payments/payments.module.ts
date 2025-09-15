b27f861b3fb9de7daa280a586ae79c7c5002397b
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 14:51:05 +0100

    Update payments.module.ts

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
+import { Wallet } from '../wallet/entities/wallet.entity';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Wallet])],
   providers: [PaymentsService],
   controllers: [PaymentsController],
   exports: [PaymentsService],
