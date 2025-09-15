8dbd537e84a0bc44d759be74cb609aa4fb07e7fd
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-15 19:56:04 +0100

    Update home-member.entity.ts

--- src/home/entities/home-member.entity.ts
+++ src/home/entities/home-member.entity.ts
@@ -1,5 +1,12 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
+import { Home } from './home.entity';
+
+export enum HomeRole {
+  OWNER = 'OWNER',
+  ADMIN = 'ADMIN',
+  MEMBER = 'MEMBER',
+}
 
 @Entity()
 export class HomeMember {
@@ -9,6 +16,12 @@ export class HomeMember {
   @Column()
   name!: string;
 
+  @Column({ type: 'enum', enum: HomeRole, default: HomeRole.MEMBER })
+  role!: HomeRole;
+
   @ManyToOne(() => User, (user) => user.homeMembers, { onDelete: 'CASCADE' })
   user!: User;
+
+  @ManyToOne(() => Home, (home) => home.members, { onDelete: 'CASCADE' })
+  home!: Home;
 }
