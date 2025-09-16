8113e5cb392f6b5f519428be78ce7e8880e98761
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-16 18:45:37 +0100

    Update group.entity.ts

--- src/community/entities/group.entity.ts
+++ src/community/entities/group.entity.ts
@@ -1,16 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+// src/community/entities/group.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 
-@Entity()
+@Entity('groups')
 export class Group {
-  @PrimaryGeneratedColumn()
-  id!: number;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
   @Column()
   name!: string;
 
-  @Column({ default: 0 })
+  @Column({ type: 'int', default: 0 })
   members!: number;
 
   @Column({ default: false })
   joined!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
 }
