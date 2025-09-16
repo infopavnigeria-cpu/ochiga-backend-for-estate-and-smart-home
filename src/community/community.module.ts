738633b5134c2790603c5305b368cae67ae7d3e8
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-16 17:01:57 +0100

    Update community.module.ts

--- src/community/community.module.ts
+++ src/community/community.module.ts
@@ -5,9 +5,10 @@ import { CommunityService } from './community.service';
 import { Post } from './entities/post.entity';
 import { Group } from './entities/group.entity';
 import { Comment } from './entities/comment.entity';
+import { Message } from './entities/message.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, Group, Comment])],
+  imports: [TypeOrmModule.forFeature([Post, Group, Comment, Message])],
   controllers: [CommunityController],
   providers: [CommunityService],
 })
