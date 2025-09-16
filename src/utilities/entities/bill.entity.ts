3fa215e45f69b4c4bd467c13dc0fe2e47e43cc8f
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-16 16:41:22 +0100

    Update bill.entity.ts

--- src/utilities/entities/bill.entity.ts
+++ src/utilities/entities/bill.entity.ts
@@ -1,16 +1,20 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity('bills')
+@Entity()
 export class Bill {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column()
-  name: string;
+  name!: string;
 
   @Column()
-  amount: string; // you can use string for ₦ formatting or number if you’ll calculate
+  amount!: string;
 
-  @Column({ default: 'Unpaid' })
-  status: 'Paid' | 'Unpaid';
+  @Column({
+    type: 'enum',
+    enum: ['Paid', 'Unpaid'],
+    default: 'Unpaid',
+  })
+  status!: 'Paid' | 'Unpaid';
 }
