e499335287cb4bda17ce418ff8f93acaf0f9b113
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-16 10:15:50 +0100

    Update wallet.entity.ts

--- src/wallet/entities/wallet.entity.ts
+++ src/wallet/entities/wallet.entity.ts
@@ -3,6 +3,7 @@ import {
   PrimaryGeneratedColumn,
   Column,
   OneToOne,
+  OneToMany,
   JoinColumn,
   CreateDateColumn,
   UpdateDateColumn,
@@ -15,17 +16,20 @@ export class Wallet {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
+  // Store as string to avoid float precision issues
   @Column('decimal', { precision: 12, scale: 2, default: 0 })
-  balance!: number;
+  balance!: string;
 
   @Column({ default: 'NGN' })
   currency!: string;
 
+  // A user has exactly one wallet
   @OneToOne(() => User, (user) => user.wallet, { onDelete: 'CASCADE' })
   @JoinColumn()
   user!: User;
 
-  @OneToOne(() => Payment, (payment) => payment.wallet)
+  // A wallet can have many payments
+  @OneToMany(() => Payment, (payment) => payment.wallet)
   payments!: Payment[];
 
   @CreateDateColumn()
