3bebe7482c3501b6dd3271df68c039ab6ae656b8
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-16 19:28:46 +0100

    Update roles.decorator.ts

--- src/auth/roles.decorator.ts
+++ src/auth/roles.decorator.ts
@@ -1,6 +1,6 @@
 // src/auth/roles.decorator.ts
 import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../enums/user-role.enum'; // ✅ fixed import
+import { UserRole } from '../enums/user-role.enum';
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
