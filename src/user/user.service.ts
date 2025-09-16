4efadf0fe6f7942c044bbbbdce65bc055e487551
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-16 11:36:08 +0100

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { UserRole } from './enums/user-role.enum';
+import { UserRole } from '../enums/user-role.enum';   // ✅ fixed
 
 @Injectable()
 export class UserService {
