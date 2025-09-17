9a2b2703256f478575a9434718daae2e05f009f3
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-18 00:41:42 +0100

    Update dashboard.module.ts

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,8 +1,10 @@
-// src/dashboard/dashboard.module.ts
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
+import { DashboardService } from './dashboard.service';
 
 @Module({
   controllers: [DashboardController],
+  providers: [DashboardService],
+  exports: [DashboardService], // ✅ Export if other modules will use it
 })
 export class DashboardModule {}
