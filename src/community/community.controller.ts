a79d60644463085b3ad478ee0b2ddb3f0bb7d526
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-18 00:30:44 +0100

    Update community.controller.ts

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -1,28 +1,60 @@
+import { Controller, Get, Post, Body, Param, Patch, Query } from '@nestjs/common';
+import { CommunityService } from './community.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { CreateMessageDto } from './dto/create-message.dto';
 
-// Posts
-@HttpPost('posts')
-createPost(@Body() data: CreatePostDto) {
-  return this.communityService.createPost(data);
-}
+@Controller('community')
+export class CommunityController {
+  constructor(private readonly communityService: CommunityService) {}
 
-// Comments
-@HttpPost('posts/:id/comments')
-addComment(@Param('id') id: string, @Body() data: CreateCommentDto) {
-  return this.communityService.addComment(id, data);
-}
+  // Posts
+  @Post('posts')
+  createPost(@Body() data: CreatePostDto) {
+    return this.communityService.createPost(data);
+  }
 
-// Groups
-@HttpPost('groups')
-createGroup(@Body() data: CreateGroupDto) {
-  return this.communityService.createGroup(data);
-}
+  @Get('posts')
+  getPosts() {
+    return this.communityService.findAllPosts();
+  }
 
-// Messages
-@HttpPost('messages')
-createMessage(@Body() data: CreateMessageDto) {
-  return this.communityService.createMessage(data);
+  @Patch('posts/:id/like')
+  likePost(@Param('id') id: string) {
+    return this.communityService.likePost(id);
+  }
+
+  // Comments
+  @Post('posts/:id/comments')
+  addComment(@Param('id') id: string, @Body() data: CreateCommentDto) {
+    return this.communityService.addComment(id, data);
+  }
+
+  // Groups
+  @Post('groups')
+  createGroup(@Body() data: CreateGroupDto) {
+    return this.communityService.createGroup(data);
+  }
+
+  @Get('groups')
+  getGroups() {
+    return this.communityService.findAllGroups();
+  }
+
+  @Patch('groups/:id/toggle')
+  toggleJoinGroup(@Param('id') id: string) {
+    return this.communityService.toggleJoinGroup(id);
+  }
+
+  // Messages
+  @Post('messages')
+  createMessage(@Body() data: CreateMessageDto) {
+    return this.communityService.createMessage(data);
+  }
+
+  @Get('messages/conversation')
+  getConversation(@Query('user1') user1: string, @Query('user2') user2: string) {
+    return this.communityService.getConversation(user1, user2);
+  }
 }
