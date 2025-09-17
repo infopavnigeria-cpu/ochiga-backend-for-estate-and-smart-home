2bbe3027ab26b6d79c57699b4943294c79fcf277
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-17 17:49:52 +0100

    Update fund-wallet.dto.ts

--- src/wallet/dto/fund-wallet.dto.ts
+++ src/wallet/dto/fund-wallet.dto.ts
@@ -1,1 +1,10 @@
+// src/wallet/dto/fund-wallet.dto.ts
+import { IsUUID, IsNumber } from 'class-validator';
 
+export class FundWalletDto {
+  @IsUUID()
+  userId!: string;
+
+  @IsNumber()
+  amount!: number;
+}
