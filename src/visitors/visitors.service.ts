7ff188551220e89031996c5f3e3a7591fcf9135c
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-17 07:36:07 +0100

    Update visitors.service.ts

--- src/visitors/visitors.service.ts
+++ src/visitors/visitors.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Visitor } from './entities/visitor.entity';
+import { Visitor } from './entities/visitors.entity';
 
 @Injectable()
 export class VisitorsService {
