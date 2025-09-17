994e7e146f26c60434bb4fc734112a92fcd2acc0
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-17 17:50:59 +0100

    Update debit-wallet.dto.ts

--- src/wallet/dto/debit-wallet.dto.ts
+++ src/wallet/dto/debit-wallet.dto.ts
@@ -1,1 +1,10 @@
+// src/wallet/dto/debit-wallet.dto.ts
+import { IsUUID, IsNumber } from 'class-validator';
 
+export class DebitWalletDto {
+  @IsUUID()
+  userId!: string;
+
+  @IsNumber()
+  amount!: number;
+}
