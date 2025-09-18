a327b07ca6ca851a651791fbf5d58f4f9e85ba7d
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-18 10:55:53 +0100

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,35 +1,12 @@
 // src/data-source.ts
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./user/entities/user.entity";
-import { Resident } from "./user/entities/resident.entity";
-import { Estate } from "./estate/entities/estate.entity";
-import { Home } from "./home/entities/home.entity";
-import { Room } from "./room/entities/room.entity";
-import { HomeMember } from "./home/entities/home-member.entity";
-import { Wallet } from "./wallet/entities/wallet.entity";
-import { Visitor } from "./visitors/entities/visitors.entity";
-import { Payment } from "./payments/entities/payment.entity";
-import { Device } from "./iot/entities/device.entity";
-import { DeviceLog } from "./iot/entities/device-log.entity";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "db.sqlite",
-  entities: [
-    User,
-    Resident,
-    Estate,
-    Home,
-    Room,
-    HomeMember,
-    Wallet,
-    Visitor,
-    Payment,
-    Device,      // 👈 add Device
-    DeviceLog,   // 👈 add DeviceLog
-  ],
-  migrations: ["src/migrations/*.ts"],
+  entities: [__dirname + "/**/*.entity.{ts,js}"], // 👈 auto-load all entities
+  migrations: [__dirname + "/migrations/*.{ts,js}"],
   synchronize: false, // keep false for migrations
   logging: true,
 });
