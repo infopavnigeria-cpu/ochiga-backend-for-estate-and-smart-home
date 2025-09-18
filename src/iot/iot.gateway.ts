de83679a03abecbac3b462c80f843229fcd31e73
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-18 08:26:56 +0100

    Update iot.gateway.ts

--- src/iot/iot.gateway.ts
+++ src/iot/iot.gateway.ts
@@ -1,3 +1,26 @@
 // src/iot/iot.gateway.ts
-this.server.to(device.owner.id).emit('deviceUpdated', device); // Resident-only
-this.server.to('managers').emit('estateDeviceUpdated', device); // Estate-wide
+import {
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { Device } from './entities/device.entity';
+
+@WebSocketGateway()
+export class IotGateway {
+  @WebSocketServer()
+  server: Server;
+
+  // Call this method whenever a device is updated
+  notifyDeviceUpdate(device: Device) {
+    if (device.owner) {
+      // Resident-only notification
+      this.server.to(device.owner.id).emit('deviceUpdated', device);
+    }
+
+    if (device.isEstateLevel) {
+      // Estate-wide notification to all managers
+      this.server.to('managers').emit('estateDeviceUpdated', device);
+    }
+  }
+}
