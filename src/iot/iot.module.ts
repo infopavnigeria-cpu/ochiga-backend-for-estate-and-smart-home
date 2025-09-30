e631a7aeb70438bb19f20541ef8c31ac0e35ff1a
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-30 02:05:41 +0100

    Update iot.module.ts

--- src/iot/iot.module.ts
+++ src/iot/iot.module.ts
@@ -1,14 +1,17 @@
+// src/iot/iot.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IotController } from './iot.controller';
 import { IotService } from './iot.service';
 import { Device } from './entities/device.entity';
 import { DeviceLog } from './entities/device-log.entity';
+import { IotGateway } from './iot.gateway';
+import { IotMqttService } from './iot.mqtt.service'; // ✅ fixed filename
 
 @Module({
   imports: [TypeOrmModule.forFeature([Device, DeviceLog])],
   controllers: [IotController],
-  providers: [IotService],
-  exports: [IotService], // in case we need it elsewhere
+  providers: [IotService, IotGateway, IotMqttService],
+  exports: [IotService],
 })
 export class IotModule {}
