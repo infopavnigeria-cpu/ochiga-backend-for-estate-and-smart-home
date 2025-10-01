4cc890b549ec947b02a0d5c8ac0422875f8021d2
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-10-01 12:15:10 +0100

    Update user.entity.ts

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,64 +1,29 @@
-// src/user/entities/user.entity.ts
+// src/iot/entities/device.entity.ts
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
-  OneToOne,
-  OneToMany,
+  ManyToOne,
 } from 'typeorm';
-import { Wallet } from '../../wallet/entities/wallet.entity';
-import { Payment } from '../../payments/entities/payment.entity';
-import { Visitor } from '../../visitors/entities/visitors.entity';
-import { HomeMember } from '../../home/entities/home-member.entity';
-import { Resident } from './resident.entity';
-import { UserRole } from '../../enums/user-role.enum';
-import { Device } from '../../iot/entities/device.entity';
-import { Notification } from '../../notifications/entities/notification.entity';
+import { User } from '../../user/entities/user.entity';
 
-@Entity('users')
-export class User {
+@Entity('devices')
+export class Device {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
-
-  @Column({ unique: true })
-  email!: string;
-
-  @Column()
-  password!: string;
 
   @Column()
   name!: string;
 
-  @Column({ type: 'text', default: UserRole.RESIDENT })
-  role!: UserRole;
+  @Column()
+  type!: string; // e.g., "light", "thermostat"
 
-  @OneToOne(() => Wallet, (wallet) => wallet.user, {
-    cascade: true,
-    eager: true,
+  @Column({ default: false })
+  status!: boolean;
+
+  // 👇 Add this relation to fix "owner not found"
+  @ManyToOne(() => User, (user) => user.devices, {
+    onDelete: 'CASCADE',
   })
-  wallet!: Wallet;
-
-  @OneToMany(() => Payment, (payment) => payment.user)
-  payments!: Payment[];
-
-  @OneToMany(() => Visitor, (visitor) => visitor.invitedBy, {
-    cascade: true,
-  })
-  invitedVisitors!: Visitor[];
-
-  @OneToMany(() => HomeMember, (member) => member.user)
-  homeMembers!: HomeMember[];
-
-  @OneToMany(() => Resident, (r) => r.user)
-  residentRecords!: Resident[];
-
-  // 🔑 Devices owned by this user
-  @OneToMany(() => Device, (device) => device.owner)
-  devices!: Device[];
-
-  // 🔔 Notifications for this user
-  @OneToMany(() => Notification, (notification) => notification.user, {
-    cascade: true,
-  })
-  notifications!: Notification[];
+  owner!: User;
 }
