9ff28186c80be31d4020605bb2d3e99b0f5e1b2e
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-10-19 12:23:42 +0100

    Update health.controller.ts

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,3 @@
-// src/health/health.controller.ts
 import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheck,
@@ -23,13 +22,8 @@ export class HealthController {
   @HealthCheck()
   check() {
     return this.health.check([
-      // ✅ DB check (timeout after 300ms)
       () => this.db.pingCheck('database', { timeout: 300 }),
-
-      // ✅ Memory heap check (limit 300MB)
       () => this.memory.checkHeap('memory_heap', 300 * 1024 * 1024),
-
-      // ✅ Ping google.com to confirm external internet
       () => this.http.pingCheck('google', 'https://www.google.com'),
     ]);
   }
