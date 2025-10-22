bc52b1d98464c9decc373516ea338953bad6623f
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-10-22 14:48:43 +0100

    Update message.module.ts

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,12 +1,14 @@
+// src/message/message.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
+import { AwsIotService } from './aws-iot.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Message])],
-  providers: [MessageService],
+  providers: [MessageService, AwsIotService],
   controllers: [MessageController],
 })
 export class MessageModule {}
