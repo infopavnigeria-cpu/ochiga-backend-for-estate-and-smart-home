40f4683dba019a066216a6ffdd520b9ff0b5b61f
infopavnigeria-cpu <info.pavnigeria@gmail.com>
2025-10-30 15:57:46 +0100

    Update iot.module.ts

--- src/iot/iot.module.ts
+++ src/iot/iot.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IotController } from './iot.controller';
 import { IotService } from './iot.service';
-import { Device } from './entities/device.entity';
-import { DeviceLog } from './entities/device-log.entity';
 import { IotGateway } from './iot.gateway';
 import { IotMqttService } from './iot.mqtt';
+import { Device } from './entities/device.entity';
+import { DeviceLog } from './entities/device-log.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Device, DeviceLog])],
