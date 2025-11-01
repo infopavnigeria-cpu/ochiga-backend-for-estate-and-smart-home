c0f425cae9057ac4ab64d58deeed869b9261b4a5
infopavnigeria-cpu <info.pavnigeria@gmail.com>
2025-11-01 07:24:37 +0100

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,3 @@
-// src/user/user.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
@@ -7,11 +6,24 @@ import { User } from './entities/user.entity';
 import { Resident } from './entities/resident.entity';
 import { ResidentController } from './resident.controller';
 import { ResidentService } from './resident.service';
+import { AiModule } from '../ai/ai.module'; // ✅ Import the AI module so AiAgent can be injected
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Resident])],
-  controllers: [UserController, ResidentController],
-  providers: [UserService, ResidentService],
-  exports: [UserService, ResidentService],
+  imports: [
+    TypeOrmModule.forFeature([User, Resident]), // Database entities
+    AiModule, // ✅ Add AiModule here to provide AiAgent
+  ],
+  controllers: [
+    UserController,
+    ResidentController,
+  ],
+  providers: [
+    UserService,
+    ResidentService,
+  ],
+  exports: [
+    UserService,
+    ResidentService,
+  ],
 })
 export class UserModule {}
