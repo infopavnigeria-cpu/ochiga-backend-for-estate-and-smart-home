b7c931c013184c310fb9d0d38c0efcc196e5b357
infopavnigeria-cpu <info.pavnigeria@gmail.com>
2025-11-03 19:00:45 +0100

    Create command.entity.ts

---
+++ src/assistant/command.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class Command {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  text: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
